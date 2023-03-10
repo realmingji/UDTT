9c22d2eaad02be25d4019ed2281e20c7a80b3299
sung-eunj <racer.sungeun@gmail.com>
2023-03-10 16:07:19 +0900

    Feat : app.js 일부 코드 수정

--- back/src/app.js
+++ back/src/app.js
@@ -1,16 +1,23 @@
 // express 받아오기
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 
+const app = express();
 app.use(cors()); //CORS 방지
 app.use(morgan("dev"));
 
+//body parser
+// Content-Type: application/json 형태의 데이터를 인식하고 핸들링
 // Content-Type: application/x-www-form-urlencoded 형태의 데이터를 인식하고 핸들링
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Content-Type: application/json 형태의 데이터를 인식하고 핸들링
-app.use(express.json());
+//메인 페이지
+
+//router, service 구현
+
+//에러처리 미들웨어
+
 
 module.exports = { app };
