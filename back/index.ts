2d6d9a191ecec8f7eaa3d5bf57c39566e82b754c
hera317088@gmail.com <HERA317088@gmail.com>
2023-03-12 17:33:19 +0900

    Feat : google OAuth index.ts file 수정

--- back/index.ts
+++ back/index.ts
@@ -1,5 +1,6 @@
 //0311 google OAuth2.0 login 구현 추가
 
+// 필요 module import, 엔진 설정
 import express from 'express';
 import {google} from 'googleapis';
 
@@ -9,18 +10,22 @@ app.set('view engine', 'pug');
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+// 클라이언트 ID, 클라이언트 암호 및 리디렉션 URI로 oauth2Client 설정
+// redirectUri는 Google 개발자 콘솔의 리디렉션 URI 승인에서 설정한 것과 동일 해야함
 const oauth2Client = new google.auth.OAuth2(
     'Client Id',
     'Client Secret',
     'http://localhost:8080/auth/google/callback'
 );
-
+// redirectUrl을 생성, redirectUrl은 앱을 Gmail 계정으로 리디렉션
+// scope 배열에 필요한 요소만 범위 목록에 포함하기
 const redirectUrl = oauth2Client.generateAuthUrl({
     access_type: 'offline',
     prompt: 'consent',
     scope: ['email', 'profile']
 });
 
+// 로그인 페이지로 이동
 let auth = false;
 
 app.get('/', async function (req, res) {
@@ -33,6 +38,7 @@ app.get('/', async function (req, res) {
     }
 });
 
+// Google access_token 및 refresh_token을 가져온 다음 oauth2Client로 설정
 app.get('/auth/google/callback', async function (req, res) {
     const code = req.query.code;
     if (code) {
