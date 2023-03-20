8d18ef9e5f3a64e59907fcbfc38abcf00986d861
ErinSY <seoyul0203@gmail.com>
2023-03-20 12:41:31 +0900

    Router + Component 수정

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './pages/Layout/navbar/NavBar';
-import Footer from './pages/Layout/footer/Footer';
-import Main from './pages/main/Main';
-import MyPage from './pages/mypage/Mypage';
-import Login from './pages/login/Login';
-import SignUp from './pages/signup/SignUp';
-import CommunityMain from './pages/community/CommunityMain';
-import CommunityWrite from './pages/community/CommunityWrite';
-import CommunityDetail from './pages/community/CommunityDetail';
+import { ROUTE_ARR } from './routes';
+import { Routes, Route } from 'react-router-dom';
+import Nav from './components/navbar/NavBar';
+import Footer from './components/footer/Footer';
+// import Nav from './pages/Layout/navbar/NavBar';
+// import Footer from './pages/Layout/footer/Footer';
+// import Main from './pages/main/Main';
+// import MyPage from './pages/mypage/Mypage';
+// import Login from './pages/login/Login';
+// import SignUp from './pages/signup/SignUp';
+// import CommunityMain from './pages/community/CommunityMain';
+// import CommunityWrite from './pages/community/CommunityWrite';
+// import CommunityDetail from './pages/community/CommunityDetail';
 
 /*
   회원가입 : /register
@@ -26,21 +29,41 @@ import CommunityDetail from './pages/community/CommunityDetail';
   댓글 수정 : /user/comments/:commentsId
   댓글 삭제 : /user/comments/:commentsId
 */
+// const Router = () => {
+//   return (
+//     <BrowserRouter>
+//       <Nav />
+//       <Routes>
+//         <Route path="/" element={<Main />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/register" element={<SignUp />} />
+//         <Route path="/users/:userId" element={<MyPage />} />
+//         <Route path="/users/groups" element={<CommunityMain />} />
+//         <Route path="/users/groups/:groupId" element={<CommunityDetail />} />
+//         <Route path="/users/groups/new" element={<CommunityWrite />} />
+//       </Routes>
+//       <Footer />
+//     </BrowserRouter>
+//   );
+// };
+
+// export default Router;
+
+/// 코드리뷰반영 ///
+
 const Router = () => {
   return (
-    <BrowserRouter>
+    <>
       <Nav />
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<SignUp />} />
-        <Route path="/users/:userId" element={<MyPage />} />
-        <Route path="/users/groups" element={<CommunityMain />} />
-        <Route path="/users/groups/:groupId" element={<CommunityDetail />} />
-        <Route path="/users/groups/new" element={<CommunityWrite />} />
+        {ROUTE_ARR.map((route, index) => {
+          return (
+            <Route path={route.path} element={<route.element />} key={index} />
+          );
+        })}
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 };
 
---
+++ front/src/routes.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Main from './pages/main/Main';
+import MyPage from './pages/mypage/Mypage';
+import Login from './pages/login/Login';
+// import SignUp from './pages/signup/SignUp';
+import CommunityMain from './pages/community/CommunityMain';
+import CommunityWrite from './pages/community/CommunityWrite';
+import CommunityDetail from './pages/community/CommunityDetail';
+
+/// 코드리뷰 반영 ///
+
+export const ROUTE = {
+  MAIN: {
+    path: '/',
+    link: '/',
+    element: Main,
+  },
+  LOGIN: {
+    path: '/login',
+    link: '/login',
+    element: Login,
+  },
+  MYPAGE: {
+    path: '/users/:userId',
+    link: '/users/:userId',
+    element: MyPage,
+  },
+  COMMUNITYMAIN: {
+    path: '/users/groups',
+    link: '/users/groups',
+    element: CommunityMain,
+  },
+  COMMUNITYDETAIL: {
+    path: '/users/groups/:groupId',
+    link: '/users/groups/:groupId',
+    element: CommunityDetail,
+  },
+  COMMUNITYWRITE: {
+    path: '/users/groups/new',
+    link: '/users/groups/new',
+    element: CommunityWrite,
+  },
+};
+
+export const ROUTE_ARR = Object.values(ROUTE);
