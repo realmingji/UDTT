9186a6fd7d651942f5a7a7a47fba52ce704b392e
sung-eunj <racer.sungeun@gmail.com>
2023-03-22 02:49:37 +0900

    Create : group 스키마 생성

---
+++ back/src/db/schemas/groupSchema.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const GroupSchema = new mongoose.Schema(
+  {
+    groupId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    info: {
+      type: String,
+      required: true,
+    },
+    startTime: {
+      type: Date,
+      required: true,
+    },
+    endTime: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['Open', 'Close'],
+    },
+    leaderID: {
+      type: Schema.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    collection: 'groups',
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model('groups', GroupSchema);
