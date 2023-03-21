6fac234672391e9d3999a18b0c25118397885835
minse317 <s2019w30@e-mirim.hs.kr>
2023-03-21 23:35:42 +0900

    feat, create: 마커 생성, 지도 수정

--- front/src/pages/community/Map.jsx
+++ front/src/pages/community/Map.jsx
@@ -1,25 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { markerdata } from './data/MapData';
 
 const { kakao } = window;
 
-const Map = () => {
+export default function Map() {
   useEffect(() => {
-    var container = document.getElementById('map');
-    var options = {
-      center: new kakao.maps.LatLng(37.524823, 126.980454),
-      level: 8,
-    };
-    var map = new kakao.maps.Map(container, options);
+    mapscript();
   }, []);
 
-  return (
-    <div>
-      <div
-        id="map"
-        style={{ width: '1000px', height: '600px', margin: 'auto' }}
-      ></div>
-    </div>
-  );
-};
+  const mapscript = () => {
+    let container = document.getElementById('map');
+    let options = {
+      center: new kakao.maps.LatLng(37.535865538724074, 126.97672632876493),
+      level: 8,
+    };
 
-export default Map;
+    //map
+    const map = new kakao.maps.Map(container, options);
+
+    markerdata.forEach(el => {
+      new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(el.lat, el.lng),
+        title: el.title,
+      });
+    });
+  };
+
+  return (
+    <div
+      id="map"
+      style={{
+        width: '1000px',
+        height: '500px',
+        margin: 'auto',
+        marginBottom: '20px',
+      }}
+    ></div>
+  );
+}
