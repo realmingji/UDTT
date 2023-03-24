035589a270490e3454be7f5b55e06e5a19bdb86b
ErinSY <seoyul0203@gmail.com>
2023-03-24 19:49:38 +0900

    fix: token값 받아오기 코드 수정

--- front/src/pages/mypage/Mypage.jsx
+++ front/src/pages/mypage/Mypage.jsx
@@ -14,7 +14,7 @@ export default function MyPage() {
   const [joinList, setJoinList] = useState([]);
   const [myClubList, setMyClubList] = useState([]);
   const [myCommentList, setMyCommentList] = useState([]);
-  const userId = JSON.parse(localStorage.getItem('userData'))._id;
+  const userId = JSON.parse(localStorage.getItem('token'));
 
   // [ 사용자정보가져오기 ]
   // useEffect(() => {
